19f66555bed23ef64265555918fb6ae3cf07ed0b
berrualakoc-debug <berrualakoc@gmail.com>
2026-09-24 17:35:50 +0300

    imgs

---
+++ data.js
@@ -0,0 +1,401 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-giri",
+      "name": "Giriş",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2575488212953898
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0819773392594989,
+          "pitch": 0.4203982154807555,
+          "rotation": 0,
+          "target": "1-galeri-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-galeri-",
+      "name": "Galeri ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.2268928027592647,
+        "pitch": 0.04395880299271937,
+        "fov": 1.2575488212953898
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7006491309703158,
+          "pitch": 0.36301443766896035,
+          "rotation": 0.7853981633974483,
+          "target": "3-asma-kat-"
+        },
+        {
+          "yaw": 0.3276439036435512,
+          "pitch": 0.40426224425107193,
+          "rotation": 0,
+          "target": "2-frn-alan"
+        },
+        {
+          "yaw": 3.0936408034838347,
+          "pitch": 0.3807939362184847,
+          "rotation": 0,
+          "target": "0-giri"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-frn-alan",
+      "name": "Fırın alanı",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2967648059562737,
+          "pitch": 0.7120362761965424,
+          "rotation": 0,
+          "target": "1-galeri-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-asma-kat-",
+      "name": "Asma Kat ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.442724637025659,
+          "pitch": 0.2989787813482945,
+          "rotation": 0,
+          "target": "4-asma-kat-2"
+        },
+        {
+          "yaw": 1.7615275007721856,
+          "pitch": 0.8224500699269086,
+          "rotation": 5.497787143782138,
+          "target": "1-galeri-"
+        },
+        {
+          "yaw": -0.13119375338759554,
+          "pitch": 0.40033482074894877,
+          "rotation": 7.853981633974483,
+          "target": "8-mescit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-asma-kat-2",
+      "name": "Asma Kat 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.92905218644621,
+        "pitch": 0.1747128896761545,
+        "fov": 1.2575488212953898
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8980207656290311,
+          "pitch": 0.47996888449698716,
+          "rotation": 0,
+          "target": "5-asma-kat-3"
+        },
+        {
+          "yaw": 2.416418546788228,
+          "pitch": 0.3233375459100092,
+          "rotation": 0,
+          "target": "3-asma-kat-"
+        },
+        {
+          "yaw": 1.9147064242003573,
+          "pitch": 0.6136445581798249,
+          "rotation": 4.71238898038469,
+          "target": "8-mescit"
+        },
+        {
+          "yaw": -2.683295004787647,
+          "pitch": 0.6172757521197987,
+          "rotation": 4.71238898038469,
+          "target": "6-bay-wc"
+        },
+        {
+          "yaw": -2.302972212675664,
+          "pitch": 0.6158399320137651,
+          "rotation": 1.5707963267948966,
+          "target": "7-bayan-wc"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-asma-kat-3",
+      "name": "Asma Kat 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2575488212953898
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05842151078740088,
+          "pitch": 0.364178385696853,
+          "rotation": 0,
+          "target": "4-asma-kat-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-bay-wc",
+      "name": "Bay WC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.7928036159885181,
+        "pitch": 0.1993204196485987,
+        "fov": 1.2575488212953898
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.943224385037361,
+          "pitch": 0.9776841745152556,
+          "rotation": 1.5707963267948966,
+          "target": "4-asma-kat-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-bayan-wc",
+      "name": "Bayan WC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.0017056282631955,
+        "pitch": 0.24192355767246987,
+        "fov": 1.2575488212953898
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1662162074083797,
+          "pitch": 1.1470790378394504,
+          "rotation": 4.71238898038469,
+          "target": "4-asma-kat-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-mescit",
+      "name": "Mescit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.037861018785426,
+          "pitch": 1.1385936072687155,
+          "rotation": 3.141592653589793,
+          "target": "4-asma-kat-2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
